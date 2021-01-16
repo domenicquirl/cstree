efa5137d700c470eb35bf88c1cdd2b8816fd0a73
Justus K <justus.k@protonmail.com>
2021-01-16 16:42:52 +0100

    Implement serialization for node data

--- src/serde_impls.rs
+++ src/serde_impls.rs
@@ -3,6 +3,7 @@
 use crate::{interning::Resolver, GreenNodeBuilder, Language, NodeOrToken, SyntaxKind, SyntaxNode, WalkEvent};
 use serde::{
     de::{SeqAccess, Visitor},
+    ser::SerializeTuple,
     Deserialize, Serialize,
 };
 use std::{fmt, marker::PhantomData};
@@ -12,7 +13,10 @@ type Rodeo = lasso::Rodeo<lasso::Spur, fxhash::FxBuildHasher>;
 #[derive(Deserialize, Serialize)]
 #[serde(tag = "t", content = "c")]
 enum Event<'text, L: Language> {
-    EnterNode(L::Kind),
+    /// The second parameter represents the data of the node.
+    /// `0` means there's no data, otherwise it's the `idx + 1`,
+    /// where `idx` is the element inside the data list.
+    EnterNode(L::Kind, u32),
     Token(L::Kind, &'text str),
     LeaveNode,
 }
@@ -21,21 +25,42 @@ impl<L, D, R> Serialize for SyntaxNode<L, D, R>
 where
     L: Language,
     <L as Language>::Kind: Serialize,
+    D: Serialize,
     R: Resolver,
 {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
+        let mut counter = 0;
+        let mut data_list = Vec::new();
+
         let events = self.preorder_with_tokens().filter_map(|event| match event {
-            WalkEvent::Enter(NodeOrToken::Node(node)) => Some(Event::<L>::EnterNode(node.kind())),
+            WalkEvent::Enter(NodeOrToken::Node(node)) => {
+                let id = node
+                    .get_data()
+                    .map(|data| {
+                        data_list.push(data);
+                        counter += 1;
+                        counter
+                    })
+                    .unwrap_or(0);
+
+                Some(Event::<L>::EnterNode(node.kind(), id))
+            }
             WalkEvent::Enter(NodeOrToken::Token(tok)) => Some(Event::Token(tok.kind(), tok.text())),
 
             WalkEvent::Leave(NodeOrToken::Node(_)) => Some(Event::LeaveNode),
             WalkEvent::Leave(NodeOrToken::Token(_)) => None,
         });
 
-        serializer.collect_seq(events)
+        let mut tuple = serializer.serialize_tuple(2)?;
+
+        // TODO(Stupremee): We can easily avoid this allocation but it would
+        // require more weird and annoying-to-write code, so I'll skip it for now.
+        tuple.serialize_element(&events.collect::<Vec<_>>())?;
+        tuple.serialize_element(&data_list)?;
+        tuple.end()
     }
 }
 
@@ -43,6 +68,7 @@ impl<'de, L, D> Deserialize<'de> for SyntaxNode<L, D, Rodeo>
 where
     L: Language,
     <L as Language>::Kind: Deserialize<'de>,
+    D: Deserialize<'de>,
 {
     fn deserialize<DE>(deserializer: DE) -> Result<Self, DE::Error>
     where
@@ -56,8 +82,9 @@ where
         where
             L: Language,
             <L as Language>::Kind: Deserialize<'de>,
+            D: Deserialize<'de>,
         {
-            type Value = SyntaxNode<L, D, Rodeo>;
+            type Value = (SyntaxNode<L, D, Rodeo>, Vec<u32>);
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
                 formatter.write_str("a list of tree events")
@@ -68,21 +95,55 @@ where
                 A: SeqAccess<'de>,
             {
                 let mut builder = GreenNodeBuilder::new();
+                let mut ids = Vec::new();
 
                 while let Some(next) = seq.next_element::<Event<'_, L>>()? {
                     match next {
-                        Event::EnterNode(kind) => builder.start_node(L::kind_to_raw(kind)),
+                        Event::EnterNode(kind, id) => {
+                            builder.start_node(L::kind_to_raw(kind));
+                            ids.push(id);
+                        }
                         Event::Token(kind, text) => builder.token(L::kind_to_raw(kind), text),
                         Event::LeaveNode => builder.finish_node(),
                     }
                 }
 
                 let (tree, resolver) = builder.finish();
-                Ok(SyntaxNode::new_root_with_resolver(tree, resolver.unwrap()))
+                let tree = SyntaxNode::new_root_with_resolver(tree, resolver.unwrap());
+                Ok((tree, ids))
             }
         }
 
-        deserializer.deserialize_seq(EventVisitor { _marker: PhantomData })
+        struct ProcessedEvents<L: Language, D: 'static>(SyntaxNode<L, D, Rodeo>, Vec<u32>);
+        impl<'de, L, D> Deserialize<'de> for ProcessedEvents<L, D>
+        where
+            L: Language,
+            <L as Language>::Kind: Deserialize<'de>,
+            D: Deserialize<'de>,
+        {
+            fn deserialize<DE>(deserializer: DE) -> Result<Self, DE::Error>
+            where
+                DE: serde::Deserializer<'de>,
+            {
+                let (tree, ids) = deserializer.deserialize_seq(EventVisitor { _marker: PhantomData })?;
+                Ok(Self(tree, ids))
+            }
+        }
+
+        let (ProcessedEvents(tree, ids), mut data) = <(ProcessedEvents<L, D>, Vec<D>)>::deserialize(deserializer)?;
+
+        let mut num_removed = 0;
+        tree.descendants().zip(ids).for_each(|(node, id)| {
+            if id == 0 {
+                return;
+            }
+
+            num_removed += 1;
+            let data = data.remove(id as usize - num_removed);
+            node.set_data(data);
+        });
+
+        Ok(tree)
     }
 }
 
--- tests/serde.rs
+++ tests/serde.rs
@@ -4,23 +4,23 @@ mod common;
 
 use common::TestLang;
 use cstree::{GreenNodeBuilder, NodeOrToken, SyntaxNode};
-use serde_test::{assert_tokens, Token::*};
+use serde_test::assert_tokens;
 
 type Rodeo = lasso::Rodeo<lasso::Spur, fxhash::FxBuildHasher>;
 
-fn build_tree() -> SyntaxNode<TestLang, (), Rodeo> {
+fn build_tree() -> SyntaxNode<TestLang, String, Rodeo> {
     let tree = common::big_tree();
     let mut builder = GreenNodeBuilder::new();
     common::build_recursive(&tree, &mut builder, 0);
     let (node, interner) = builder.finish();
 
-    SyntaxNode::<TestLang, (), _>::new_root_with_resolver(node, interner.unwrap())
+    SyntaxNode::<TestLang, _, _>::new_root_with_resolver(node, interner.unwrap())
 }
 
 /// Serializable SyntaxNode that doesn't have a identity `PartialEq` implementation,
 /// but checks if both trees have equal nodes and tokens.
 #[derive(Debug)]
-struct TestNode(SyntaxNode<TestLang, (), Rodeo>);
+struct TestNode(SyntaxNode<TestLang, String, Rodeo>);
 
 impl serde::Serialize for TestNode {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -43,6 +43,7 @@ impl<'de> serde::Deserialize<'de> for TestNode {
 impl PartialEq<TestNode> for TestNode {
     fn eq(&self, other: &TestNode) -> bool {
         self.0.kind() == other.0.kind()
+            && self.0.get_data() == other.0.get_data()
             && self.0.text_range() == other.0.text_range()
             && self
                 .0
@@ -61,34 +62,60 @@ impl PartialEq<TestNode> for TestNode {
 }
 
 #[test]
+fn serialize_json_big_tree() {
+    let tree = TestNode(build_tree());
+    tree.0.children().enumerate().for_each(|(idx, node)| {
+        node.set_data(format!("{}", idx));
+    });
+
+    let serialized = serde_json::to_string_pretty(&tree).unwrap();
+    print!("{}\n", serialized);
+
+    let deserialized = serde_json::from_str::<TestNode>(&serialized).unwrap();
+    assert_eq!(tree, deserialized);
+}
+
+#[test]
 fn serialize_big_tree() {
+    use serde_test::Token::*;
+
     let tree = TestNode(build_tree());
 
     #[rustfmt::skip]
     assert_tokens(
         &tree,
         &[
-            Seq { len: Option::None },
+            Tuple { len: 2 },
 
+            Seq { len: Option::Some(14) },
             Struct { name: "Event", len: 2 },
             BorrowedStr("t"),
             BorrowedStr("EnterNode"),
             BorrowedStr("c"),
+            Tuple { len: 2 },
             U16(0),
+            U32(0),
+            TupleEnd,
             StructEnd,
 
             Struct { name: "Event", len: 2 },
             BorrowedStr("t"),
             BorrowedStr("EnterNode"),
             BorrowedStr("c"),
+            Tuple { len: 2 },
             U16(1),
+            U32(0),
+            TupleEnd,
             StructEnd,
 
             Struct { name: "Event", len: 2 },
             BorrowedStr("t"),
             BorrowedStr("EnterNode"),
             BorrowedStr("c"),
+            Tuple { len: 2 },
             U16(2),
+            U32(0),
+            TupleEnd,
             StructEnd,
 
             Struct { name: "Event", len: 2 },
@@ -135,7 +162,10 @@ fn serialize_big_tree() {
             BorrowedStr("t"),
             BorrowedStr("EnterNode"),
             BorrowedStr("c"),
+            Tuple { len: 2 },
             U16(6),
+            U32(0),
+            TupleEnd,
             StructEnd,
 
             Struct { name: "Event", len: 2 },
@@ -177,8 +207,12 @@ fn serialize_big_tree() {
             BorrowedStr("t"),
             BorrowedStr("LeaveNode"),
             StructEnd,
-
             SeqEnd,
+
+            Seq { len: Option::Some(0) },
+            SeqEnd,
+
+            TupleEnd,
         ],
     );
 }
